2b7a8ff3dc62770072c8a620557f6065ea1e8020
Maxim S <its3zin.main@gmail.com>
2025-01-27 19:04:06 +0300

    feat(project): ui windows

--- Assets/BProject/Scripts/TempScript.cs
+++ Assets/BProject/Scripts/TempScript.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace BProject
 {
     public class TempScript : MonoBehaviour
     {
+        [SerializeField] private UIDocument UIDocument;
+        
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-        
+            Debug.Log(UIDocument.rootVisualElement.name);
         }
 
         // Update is called once per frame
