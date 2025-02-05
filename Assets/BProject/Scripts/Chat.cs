59e68de9a0f825ed17cd4250e5d680a2fba5eb57
Maxim S <its3zin.main@gmail.com>
2025-02-06 01:53:33 +0300

    feat(project): add test scene

---
+++ Assets/BProject/Scripts/Chat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace BProject
+{
+    public class Chat
+    {
+        public event Action OnChange;
+        
+        public string MessageText => 
+            _dialogueContainer.DialogueNodeData.First(x => x.Guid == _actualGuid).DialogueText;
+
+        public List<NodeLinkData> Answers 
+            => _dialogueContainer.NodeLinks.Where(x => x.BaseNodeGuid == _actualGuid).ToList();
+        
+        private DialogueContainer _dialogueContainer;
+
+        private string _actualGuid;
+        
+        public Chat(DialogueContainer dialogueContainer)
+        {
+            _dialogueContainer = dialogueContainer;
+
+            _actualGuid = _dialogueContainer.NodeLinks[0].TargetNodeGuid;
+
+            OnChange?.Invoke();
+        }
+
+        public bool TrySelect(int index)
+        {
+            if (!Answers.Any()) return false;
+            
+            index = index < 0 ? 0 : index;
+            
+            index = index > Answers.Count - 1 ? 0 : index;
+
+            _actualGuid = Answers[index].TargetNodeGuid;
+         
+            OnChange?.Invoke();
+            
+            return true;
+        }
+    }
+}
--- Assets/BProject/Scripts/TempScript.cs
+++ Assets/BProject/Scripts/TempScript.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -5,12 +6,48 @@ namespace BProject
 {
     public class TempScript : MonoBehaviour
     {
+        private VisualElement _root;
+        
+        private Chat _chat;
+        
         [SerializeField] private UIDocument UIDocument;
+        
+        [SerializeField] private DialogueContainer _dialogue;
+
         
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         void Start()
         {
-            Debug.Log(UIDocument.rootVisualElement.name);
+            _root = UIDocument.rootVisualElement;
+            
+            _chat = new Chat(_dialogue);
+
+            Refresh();
+            
+            _chat.OnChange += Refresh;
+        }
+
+        private void Refresh()
+        {
+            _root.Q<TextElement>("message").text = _chat.MessageText;
+
+            var buttons = _root.Query<Button>(); 
+            
+            if (buttons.ToList().Any()) _root.Remove(buttons);
+            
+            for(int i = 0; i < _chat.Answers.Count; i++)
+            {
+                var button = new Button()
+                {
+                    text = _chat.Answers[i].PortName,
+                };
+
+                button.style.fontSize = 48f;
+                
+                button.clicked += () => _chat.TrySelect(i);
+                
+                _root.Add(button);
+            }
         }
 
         // Update is called once per frame
